efc2e6f97f2ccc2b1ddc704fe720f59f14309697
AgustinLuna98 <agustinalejandroluna1998@gmail.com>
2023-02-04 21:23:28 -0300

    update

--- app-portfolio/src/app/shared/shared.module.ts
+++ app-portfolio/src/app/shared/shared.module.ts
@@ -4,6 +4,9 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { BannerComponent } from './banner/banner.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { ExperienceComponent } from './experience/experience.component';
+import { EducacionComponent } from './educacion/educacion.component';
+import { HardAndSoftComponent } from './hard-and-soft/hard-and-soft.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
 
 
 
@@ -12,7 +15,10 @@ import { ExperienceComponent } from './experience/experience.component';
     NavBarComponent,
     BannerComponent,
     AboutMeComponent,
-    ExperienceComponent
+    ExperienceComponent,
+    EducacionComponent,
+    HardAndSoftComponent,
+    ProyectosComponent
   ],
   imports: [
     CommonModule
@@ -20,7 +26,10 @@ import { ExperienceComponent } from './experience/experience.component';
     NavBarComponent,
     BannerComponent,
     AboutMeComponent,
-    ExperienceComponent
+    ExperienceComponent,
+    EducacionComponent,
+    HardAndSoftComponent,
+    ProyectosComponent
   ]
 })
 export class SharedModule { }
